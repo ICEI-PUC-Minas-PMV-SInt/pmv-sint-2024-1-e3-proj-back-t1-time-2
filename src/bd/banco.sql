a04ce3df18eda407e3728f74a02d927ca543fa1a
Isabella Prado <137354714+IsabellaPrado@users.noreply.github.com>
2024-05-13 19:10:48 -0400

    Correções da etapa 2. - Atualiza o DDL do banco de dados. - Atualiza o DML do banco de dados. - Atualiza o Diagrama de classes.

--- src/bd/banco.sql
+++ src/bd/banco.sql
@@ -27,39 +27,22 @@ constraint FK_Perfil_Pessoa foreign key (Id_Pessoa) references Pessoa(Id_Pessoa)
 create table Exercicio (
 Id_Exercicio integer primary key,
 Nome varchar(60),
-Descricao varchar(60),
-Classificacao varchar(20),
-GrupoMuscular varchar(60),
-Mecanica varchar(20)
-);
-create table ItemTreino (
-Id_ItemTreino integer primary key,
-Id_Exercicio integer,
-Repeticoes integer,
 Series integer,
-constraint FK_ItemTreino_Exercicio foreign key (Id_Exercicio) references Exercicio(Id_Exercicio)
+Repeticoes integer
 );
+
 create table Treino (
 Id_Treino integer primary key,
 DataInicio date,
+Objetivo varchar(250),
+DuracaoTreino varchar(60),
 Id_Perfil integer,
 constraint FK_Treino_Perfil foreign key (Id_Perfil) references Perfil(Id_Perfil)
 );
-create table Recomendacao (
-Id_Recomendacao integer primary key,
-DataRecomendacao date,
-Id_Perfil integer,
-constraint FK_Recomendacao_Perfil foreign key (Id_Perfil) references Perfil(Id_Perfil)
-);
-create table Treino_ItemTreino (
+
+create table Treino_Exercicio (
 Id_Treino integer,
-Id_ItemTreino integer,
-constraint FK_Treino_ItemTreino_Treino foreign key (Id_Treino) references Treino(Id_Treino),
-constraint FK_Treino_ItemTreino_ItemTreino foreign Key (Id_ItemTreino) references ItemTreino(Id_ItemTreino)
-);
-create table Recomendacao_ItemTreino (
-Id_Recomendacao integer,
-Id_ItemTreino integer,
-constraint FK_Recomendacao_ItemTreino_Recomendacao foreign key (Id_Recomendacao) references Recomendacao(Id_Recomendacao),
-constraint FK_Recomendacao_ItemTreino_ItemTreino foreign key (Id_ItemTreino) references ItemTreino(Id_ItemTreino)
+Id_Exercicio integer,
+constraint FK_Treino_Exercicio_Treino foreign key (Id_Treino) references Treino(Id_Treino),
+constraint FK_Treino_Exercicio_Exercicio foreign Key (Id_Exercicio) references Exercicio(Id_Exercicio)
 );
